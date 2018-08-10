e7b91761a68273b727446942cf6bb28d6fb3d937
vendettafv <leonardofeitosa.v@gmail.com>
2018-08-10 15:54:31 -0300

    Atualização do script

--- addip.sh
+++ addip.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
-echo -n "Deseja realmente adicionar IP? [Y/n]: "
+#####################################################
+##     Script to add IPs in VPS OVH with CentOS    ##
+##                 BY: vendettafv                  ##
+#####################################################
+
+# Constante de Cores
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+GREEN="\033[0;32m" # Verde
+YELLOW="\033[1;33m" # Laranja
+RED="\033[0;31m"   # Vermelho
+NORMAL="\033[0m"   # Sem cor (Cor normal)
+
+# Start
+echo -n "${GREEN}Deseja realmente adicionar IP?${NORMAL} [Yy/n]: "
 read ADDIP
 if [[ $ADDIP = [yY] ]]; then
   START=Y
   while [ "$START" != "n" ]
   do
     # Cria o arquivo
-    echo "Digite o nome do arquivo."
-    echo -n "Ex: eth0, eth0:0 ou eth0:1 : "
+    echo "${GREEN}Digite o nome do arquivo.${NORMAL}"
+    echo -n "${RED}Ex: eth0, eth0:0 ou eth0:1${NORMAL} : "
     read ARQUIVO
-    echo -n "Digite o IP: "
+    echo -n "${RED}Digite o IP:${NORMAL} "
     read IPADDR
-    echo "Criando arquivo"
+    echo "${YELLOW}Criando arquivo${NORMAL}"
     sudo touch /etc/sysconfig/network-scripts/ifcfg-$ARQUIVO
     # Add Info no arquivo
     IP="\"$IPADDR\""
@@ -22,9 +42,10 @@ BOOTPROTO="\"none\""
 IPADDR="$IP"
 NETMASK="\"255.255.255.255\""
 BROADCAST="$IP"" > /etc/sysconfig/network-scripts/ifcfg-$ARQUIVO
-    echo -n "Deseja realmente adicionar novo IP? [Y/n]: "
+    echo -n "${GREEN}Deseja realmente adicionar novo IP?${NORMAL} [Yy/n]: "
     read START
-    echo "Reiniciando Network"
+    echo "${YELLOW}Reiniciando Network${NORMAL}"
     sudo service network restart
   done
 fi
+echo "${GREEN}Precesso finalizado!${NORMAL}"
